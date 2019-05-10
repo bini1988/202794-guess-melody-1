2582e6858e11d1c7177c03184e05d66e5b6d0bca
Nikolai <rus.nick@mail.ru>
2019-05-10 14:49:57 +0300

    App screen switch logic

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {TrackGenres, QuestionTypes} from "../../types.d";
 import App from "./app";
 
 it(`App correctly renders default markup`, () => {
+  const questionsMock = [
+    {
+      type: QuestionTypes.Genre,
+      genre: TrackGenres.Rock,
+      answers: [
+        {src: `src#1`, genre: TrackGenres.Rock},
+        {src: `src#2`, genre: TrackGenres.Jazz},
+      ]
+    }, {
+      type: QuestionTypes.Artist,
+      song: {artist: `artist#1`, src: `src#1`},
+      answers: [
+        {picture: `picture#1`, artist: `artist#1`},
+        {picture: `picture#2`, artist: `artist#2`},
+      ]
+    }
+  ];
   const tree = renderer
     .create(
-        <App/>
+        <App
+          maxTime={5}
+          maxMistakes={3}
+          questions={questionsMock}/>
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
+import questions from "./mocks/questions";
+import {maxTime, maxMistakes} from "./mocks/settings";
 
 ReactDOM.render(
-    <App/>,
+    <App
+      maxTime={maxTime}
+      maxMistakes={maxMistakes}
+      questions={questions}/>,
     document.querySelector(`.main`)
 );
