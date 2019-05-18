9ac90b7929a3468e357f104a85b39a84d06ebf15
Nikolai <rus.nick@mail.ru>
2019-05-18 22:20:08 +0300

    Update mocks data

--- src/mocks/game.js
+++ src/mocks/game.js
@@ -3,6 +3,24 @@ export const maxTime = 5;
 export const maxMistakes = 3;
 export const questions = [
   {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/db/Gimn_Sovetskogo_Soyuza_%281977_Vocal%29.oga`,
+    },
+    answers: [
+      {
+        picture: `https://i.pravatar.cc/134x134`,
+        artist: `John Snow`,
+      }, {
+        picture: `https://i.pravatar.cc/134x134`,
+        artist: `Jack Daniels`,
+      }, {
+        picture: `https://i.pravatar.cc/134x134`,
+        artist: `Jim Beam`,
+      },
+    ],
+  }, {
     type: `genre`,
     genre: `rock`,
     answers: [
@@ -23,20 +41,56 @@ export const questions = [
   }, {
     type: `artist`,
     song: {
-      artist: `Jim Beam`,
+      artist: `Eminem`,
       src: `https://upload.wikimedia.org/wikipedia/commons/d/db/Gimn_Sovetskogo_Soyuza_%281977_Vocal%29.oga`,
     },
     answers: [
       {
         picture: `https://i.pravatar.cc/134x134`,
-        artist: `John Snow`,
+        artist: `Taylor Swift`,
+      }, {
+        picture: `https://i.pravatar.cc/134x134`,
+        artist: `Drake`,
+      }, {
+        picture: `https://i.pravatar.cc/134x134`,
+        artist: `Eminem`,
+      },
+    ],
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Ed Sheeran`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/d/db/Gimn_Sovetskogo_Soyuza_%281977_Vocal%29.oga`,
+    },
+    answers: [
+      {
+        picture: `https://i.pravatar.cc/134x134`,
+        artist: `Ed Sheeran`,
       }, {
         picture: `https://i.pravatar.cc/134x134`,
         artist: `Jack Daniels`,
       }, {
         picture: `https://i.pravatar.cc/134x134`,
-        artist: `Jim Beam`,
+        artist: `Bruno Mars`,
       },
     ],
-  },
+  }, {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      }, {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `blues`,
+      }, {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `jazz`,
+      }, {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+    ],
+  }
 ];
