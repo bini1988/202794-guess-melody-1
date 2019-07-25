69e959d83bd72f8d2f16b49ed6ad0fc0116be6ba
Nikolai <rus.nick@mail.ru>
2019-07-25 23:12:03 +0300

    Add GameFooter component

---
+++ src/components/game-footer/game-footer.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 10px;
+`;
+
+const Copyright = styled.section`
+  text-align: right;
+  color: #f0eed5;
+  white-space: nowrap;
+`;
+
+const Logo = styled.a`
+  &:hover, &:focus, &:active {
+    opacity: 0.8;
+  }
+`;
+
+const Text = styled.p`
+  margin: 10px 0px 0px;
+`;
+
+const Link = styled.a`
+  color: #ff9749;
+  transition: color ease-in-out 250ms;
+
+  &:hover, &:focus, &:active {
+    color: #f00000;
+  }
+`;
+
+function GameFooter() {
+  return (
+    <FooterWrapper>
+      <a
+        rel="license"
+        href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+        <img
+          src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png"
+          alt="Creative Commons License"
+          width="88"
+          height="31"
+        />
+      </a>
+      <Copyright>
+        <Logo
+          href="https://htmlacademy.ru">
+          <img
+            src="img/logo-htmla.svg"
+            alt="HTML Academy"
+            width="144"
+            height="49"
+          />
+        </Logo>
+        <Text>
+          {`Сделано в `}
+          <Link href="https://htmlacademy.ru">{`HTML Academy`}</Link>
+        </Text>
+      </Copyright>
+    </FooterWrapper>
+  );
+}
+
+export default GameFooter;
