8c8dcc8f2f350c923508dfdd88b9bf97ee9e27b3
Nikolai <rus.nick@mail.ru>
2019-07-28 20:15:52 +0300

    Update game mocks

--- src/mocks/game.js
+++ src/mocks/game.js
@@ -1,6 +1,44 @@
 
 export const maxTime = 5;
 export const maxMistakes = 3;
+export const artistQuestion = {
+  type: `artist`,
+  song: {
+    artist: `Jim Beam`,
+    src: `https://upload.wikimedia.org/wikipedia/commons/d/db/Gimn_Sovetskogo_Soyuza_%281977_Vocal%29.oga`,
+  },
+  answers: [
+    {
+      picture: `https://i.pravatar.cc/134x134`,
+      artist: `John Snow`,
+    }, {
+      picture: `https://i.pravatar.cc/134x134`,
+      artist: `Jack Daniels`,
+    }, {
+      picture: `https://i.pravatar.cc/134x134`,
+      artist: `Jim Beam`,
+    },
+  ],
+};
+export const genreQuestion = {
+  type: `genre`,
+  genre: `rock`,
+  answers: [
+    {
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      genre: `rock`,
+    },
+  ],
+};
 export const questions = [
   {
     type: `artist`,
